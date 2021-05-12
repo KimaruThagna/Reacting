5bdaf0905e393a684acfc5d99f595e8784510c8a
Kimaru thagana <macbuntu@192.168.1.3>
2021-05-12 17:24:59 +0300

    finish up app

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,26 +1,25 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log("clicked")
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
 
-    return (
-        <>
-        <header className='header'>
-            <h1>{title}</h1> 
-            <Button color='green' text='add' onClick={onClick}/>
-        </header>
-        <p>We create a melting pot of issues to be solved by your dev team so that 
-                you dont need to
-            </p>
-
-        </>
-    )
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
 
 Header.defaultProps = {
-    title:'Default Issue Tracker Title'
+  title: 'Task Tracker',
 }
+
 export default Header
