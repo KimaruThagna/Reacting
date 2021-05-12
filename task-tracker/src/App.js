ca292bce6a0cc4e0a2b8dc352068420ba4541824
Kimaru thagana <macbuntu@192.168.1.3>
2021-05-12 14:51:10 +0300

    individual tasks

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header title="Issue Tracker" />
-      
+      <Tasks/>
     </div>
   )
 }
--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import Button from './Button'
+
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log("clicked")
+    }
+
     return (
-        <header>
-            <h1>{title}</h1>
-            <p>We create a melting pot of issues to be solved by your dev team so that 
+        <>
+        <header className='header'>
+            <h1>{title}</h1> 
+            <Button color='green' text='add' onClick={onClick}/>
+        </header>
+        <p>We create a melting pot of issues to be solved by your dev team so that 
                 you dont need to
             </p>
-        </header>
+
+        </>
     )
 }
 
